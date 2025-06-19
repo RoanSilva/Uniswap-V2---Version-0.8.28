a23ecf9cb748e79a6a5c33d7218e473679491913
RoanSilva <152782438+RoanSilva@users.noreply.github.com>
2025-06-19 13:54:15 -0300

    Delete scripts directory

--- scripts/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-//# Funções auxiliares (por exemplo: wait, logging, format)
-
-const waitForTx = async (tx) => {
-  const receipt = await tx.wait();
-  console.log(`Tx confirmed in block ${receipt.blockNumber}`);
-};
